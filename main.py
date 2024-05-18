f2b4f5d649e1cb340ebd4b25872224b86ebb8599
MJahidH <m.jahid.h@hotmail.com>
2024-05-18 19:22:37 +0100

    main input password created and mode input created. Condtional logic template created based on what the user presses

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("this is the main pyton file")
+
+
+pwd = input("Please type your mster password : ")
+
+mode = input("Would you like to add a new password or add a new password (view,add)? ")
+
+if mode == "view":
+    pass 
+elif mode == "add":
+    pass 
+else:
+    print("invalid mode")
+
