9975aba618aa55788e6c2236184e1dc4dad4992c
MJahidH <m.jahid.h@hotmail.com>
2024-05-19 20:34:57 +0100

    only allows user to view passworeds if the correct master password has been typed

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from  cryptography.fernet import Fernet 
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes 
+from cryptography.hazmat.backends import default_backend
+import base64
+import os 
 
 def write_key():
     key = Fernet.generate_key()
@@ -6,18 +11,43 @@ def write_key():
         key_file.write(key) 
 
 # write_key()
-
-
 def load_key():
     file =  open("key.key","rb")
     key = file.read()
     file.close()
     return key
 
+def write_salt():
+    salt = os.urandom(16)
+    with open("salt.salt","wb") as key_file:
+        key_file.write(salt)
+
+def load_salt():
+    with open("salt.salt","rb") as salt_file:
+        salt = salt_file.read()
+        return salt 
+    
+
+    
+def get_fernet(master_pwd):
+    salt = load_salt()
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(master_pwd.encode()))
+    return Fernet(key)
+
+
+# write_salt()
+# write_key()
+
 
 master_pwd = input("Please type your mster password : ")
-key = load_key() + master_pwd.encode()
-fer = Fernet(key)
+fer = get_fernet(master_pwd)
 
 
 
